9735f256469f4692271655be3a6ae4c2757e39b0
wajmuller <waltersj@liquidthought.co.za>
2022-04-17 14:10:10 +0200

    Some more logical additions

---
+++ Packer/Converters.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packer
+{
+    public static class Converters
+    {
+        public static int TryParseInt32(this string value)
+        {
+            return TryParse<int>(value, int.TryParse);
+        }
+        public static Int16 TryParseInt16(this string value)
+        {
+            return TryParse<Int16>(value, Int16.TryParse);
+        }
+
+        public static Int64 TryParseInt64(this string value)
+        {
+            return TryParse<Int64>(value, Int64.TryParse);
+        }
+
+        public static byte TryParseByte(this string value)
+        {
+            return TryParse<byte>(value, byte.TryParse);
+        }
+
+        public static bool TryParseBoolean(this string value)
+        {
+            return TryParse<bool>(value, bool.TryParse);
+        }
+
+        public static Single TryParseSingle(this string value)
+        {
+            return TryParse<Single>(value, Single.TryParse);
+        }
+
+        public static Double TryParseDoube(this string value)
+        {
+            return TryParse<Double>(value, Double.TryParse);
+        }
+
+        public static Decimal TryParseDecimal(this string value)
+        {
+            return TryParse<Decimal>(value, Decimal.TryParse);
+        }
+
+        public static DateTime TryParseDateTime(this string value)
+        {
+            return TryParse<DateTime>(value, DateTime.TryParse);
+        }
+
+
+        private delegate bool ParseDelegate<T>(string s, out T result);
+        private static T TryParse<T>(this string value, ParseDelegate<T> parse) where T : struct
+        {
+            parse(value, out var result);
+            return result;
+        }
+    }
+}
--- Packer/Packer.cs
+++ Packer/Packer.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using Packer;
+using static System.Globalization.NumberStyles;
 
 namespace com.mobuquity.packer
 {
@@ -15,15 +18,36 @@ namespace com.mobuquity.packer
 
             try
             {
+
+                var numberStyle = NumberStyles.AllowDecimalPoint|NumberStyles.AllowCurrencySymbol;
+                var formatProvider = new CultureInfo("fr-FR")
+                {
+                    NumberFormat =
+                    {
+                        NumberDecimalSeparator = "."
+                    }
+                };
+
+                decimal ConvertDecimal(string d)
+                {
+                    if(!decimal.TryParse(d, numberStyle, formatProvider, out var res))
+                        throw new APIException("Invalid number format --" + d.GetType().Name);
+
+                    return res;
+                }
+
                 var str = File.ReadAllLines(fileLocation);
-
-
                 foreach (var line in str)
                 {
                     var items = Regex
                         .Matches(line, @"\((.*?)\)")
-                        .Select(x => x.Groups[1].Value)
-                        .ToList();
+                        .Select(x => x.Groups[1].Value?.Split(','))?
+                        .Select(x=> new PackItem
+                        {
+                            Index = x[0].TryParseInt32(),
+                            Weight =ConvertDecimal(x[1]),
+                            Cost = ConvertDecimal(x[2]),
+                        });
                 }
                 return str;
             }
@@ -35,4 +59,13 @@ namespace com.mobuquity.packer
            
         }
     }
+
+    public class PackItem
+    {
+        public int Index { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Cost { get; set; }
+    }
+
+
 }
